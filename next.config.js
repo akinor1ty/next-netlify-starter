c278337c51194d075a1b281892c34b933f82a7a2
Akinori Mizushima <akinori.mizushima@slalom.com>
2021-10-06 21:10:17 +0900

    Revert staticwebapp.config.json

--- next.config.js
+++ next.config.js
@@ -1,9 +1,3 @@
 module.exports = {
   reactStrictMode: true,
-  exportPathMap: async function () {
-    return {
-      "/": { page: "/" },
-      "/orgs/index": { page: "/orgs/[id]" },
-    };
-  },
 };
