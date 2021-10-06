127c6c953ac69c96cff0271b55b20271715cedf7
Akinori Mizushima <akinori.mizushima@slalom.com>
2021-10-06 21:00:33 +0900

    Replace routes with exportPathMap

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   reactStrictMode: true,
+  exportPathMap: async function () {
+    return {
+      "/": { page: "/" },
+      "/orgs/index": { page: "/orgs/[id]" },
+    };
+  },
 };
