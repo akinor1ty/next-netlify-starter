7eaf7124a13f61416ad19cf058236800276936a6
Akinori Mizushima <akinori.mizushima@slalom.com>
2021-10-06 20:37:28 +0900

    Add exportPathMap option

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   reactStrictMode: true,
-}
+  exportPathMap: async function () {
+    return {
+      "/": { page: "/" },
+      "/orgs/1": { page: "/orgs/[id].html" },
+    };
+  },
+};
