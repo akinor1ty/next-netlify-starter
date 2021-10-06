d1418aeed76c63ee9de186c04d6ff6c2d9e580c1
Akinori Mizushima <akinori.mizushima@slalom.com>
2021-10-06 19:18:13 +0900

    Add link button

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -14,6 +15,8 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+
+        <Link href="/about">GO TO ABOUT</Link>
 
         <p className={styles.description}>
           Get started by editing{' '}
